94069a5f9c3c4b2e046c326df23ae48f6b418e4c
plmojeresitech <plmojeres@itechnologent.ai>
2026-04-13 12:20:34 +0800

    Add files via upload

---
+++ MOURSE_COD3.ino
@@ -0,0 +1,46 @@
+int redLED = 7; 
+
+void setup() {
+	pinMode (redLED, OUTPUT); 
+}
+void loop() {
+	// dot
+	digitalWrite (redLED, HIGH);
+	delay(150);
+	digitalWrite (redLED, LOW);
+	delay(150);
+
+	//dot
+	digitalWrite (redLED, HIGH);
+	delay(150);
+	digitalWrite (redLED, LOW);
+	delay(150);
+	
+	//dot
+	digitalWrite (redLED, HIGH);
+	delay(150);
+	digitalWrite (redLED, LOW);
+	delay(150);
+
+	
+	//dash
+	digitalWrite (redLED, HIGH);
+	delay(500);
+	digitalWrite (redLED, LOW);
+	delay(500);
+
+	//dash
+	digitalWrite (redLED, HIGH);
+	delay(500);
+	digitalWrite (redLED, LOW);
+	delay(500);
+
+	//dash
+	digitalWrite (redLED, HIGH);
+	delay(500);
+	digitalWrite (redLED, LOW);
+	delay(500);
+	
+	//Adding a Gap before restarting
+	delay(1000);
+	}
